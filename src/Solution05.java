c946c9c3a86c581bb5da90e0445736f117f0a4a9
a1l1ke <onetenthplease@gmail.com>
2026-06-26 12:27:33 +0900

    Feat : 바이트 입력 스트림(FileInputStream)을 이용한 파일 읽기 실습 코드 추가

--- src/Solution05.java
+++ src/Solution05.java
@@ -1,3 +1,4 @@
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -11,6 +12,7 @@ public class Solution05 {
             System.out.println("text = " + text);
             String byteFileName = "output-byte.txt";
             saveByByteStream(text, byteFileName);
+            loadByByteStream(byteFileName);
         }
     }
 
@@ -23,6 +25,18 @@ public class Solution05 {
             // throws로 넣는 선택
             // runtime으로 던질 거냐
             // 여기서 마무리 지을 거냐
+            throw new RuntimeException(e);
+        }
+    }
+
+    static void loadByByteStream(String fileName) {
+        try (FileInputStream fis = new FileInputStream(fileName)) {
+            int b;
+            while ((b = fis.read()) != -1) {
+                System.out.print((char) b);
+            }
+            System.out.println();
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
