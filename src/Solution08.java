278d0bb38d969d6b483cd898a136fdb18f42790a
a1l1ke <onetenthplease@gmail.com>
2026-06-26 15:21:33 +0900

    Feat : 파싱된 이미지 URL 리스트의 파일 저장 기능 및 슬래시 정규화 실습 코드 추가

--- src/Solution08.java
+++ src/Solution08.java
@@ -1,9 +1,15 @@
+import java.io.BufferedWriter;
 import java.net.URI;
 import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.MatchResult;
@@ -17,8 +23,31 @@ public class Solution08 {
         List<String> urlList = getImageUrlList(keyword);
         System.out.println("urlList = " + urlList);
         // URL 리스트를 자체를 파일로 저장
+        saveUrlList(urlList, keyword);
         // 이미지를 파일 형태로 각각 다운로드
-        // github actions를 사용해서 외부에서 다운로드 받을 수 있게
+    }
+
+    private static void saveUrlList(List<String> urlList, String keyword) {
+        String fileName = "%s-%s.txt".formatted(
+//                URLEncoder.encode(keyword, StandardCharsets.UTF_8), // 가장 파일명상 안전함
+                keyword.replace(" ", "_"), // 보기 좋음
+                ZonedDateTime.now(
+                                ZoneId.of("Asia/Seoul"))
+                        .format(java.time.format.DateTimeFormatter.ofPattern(
+                                "yyyy-MM-dd_HHmmss"))
+        );
+        Path path = Paths.get(fileName);
+        try (BufferedWriter writer = Files.newBufferedWriter(path)) {
+//            writer.write(String.join("\n", urlList));
+            for (String url : urlList) {
+                // \ -> 생략해버림. \\ <- 문자로 인식함
+//                writer.write(url.replace("\\/", "/"));
+                writer.write(url);
+                writer.newLine();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private static List<String> getImageUrlList(String keyword) {
@@ -52,7 +81,8 @@ public class Solution08 {
             for (MatchResult matchResult : pattern.matcher(body).results().toList()) {
                 // ( ) <- Group(1)
                 System.out.println("matchResult = " + matchResult.group(1));
-                urlList.add(matchResult.group(1));
+//                urlList.add(matchResult.group(1));
+                urlList.add(matchResult.group(1).replace("\\/", "/"));
             }
         } catch (Exception e) {
             throw new RuntimeException(e);
